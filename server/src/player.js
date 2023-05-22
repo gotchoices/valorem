f39a924ce5f48b80893042b2b021e0a1fb4278ab
Dustin McMurry <iggle@igglepud.com>
2023-05-22 13:38:54 -0500

    edited player

--- server/src/player.js
+++ server/src/player.js
@@ -1,47 +1,35 @@
-exports.Player = class{
-
-constructor(id){
-this.id=id;
-this.legacy=0;
-this.timeLeft=0;
-this.ready=false;
-this.trades = [];
-this.holdings={
-  
-        held: {
-          con: 0,
-          dur: 0,
-          lux: 0,
-          conCap: 0,
-          durCap: 0,
-          luxCap: 0,
-        },
-        allocated: {
-          con: 0,
-          dur: 0,
-          lux: 0,
-          conCap: 0,
-          durCap: 0,
-          luxCap: 0,
-        },
-        buying: {
-          con: 0,
-          dur: 0,
-          lux: 0,
-          conCap: 0,
-          durCap: 0,
-          luxCap: 0,
-        },
-        selling: {
-          con: 0,
-          dur: 0,
-          lux: 0,
-          conCap: 0,
-          durCap: 0,
-          luxCap: 0,
-        },
-      };
-
-}
-
+exports.Player = class {
+  constructor(id){
+    this.id=id;
+    this.legacy=0;
+    this.timeLeft=0;
+    this.ready=false;
+    this.trades = [];
+    this.holdings = {
+      held: {
+        con: 0,
+        dur: 0,
+        lux: 0,
+        conCap: 0,
+        durCap: 0,
+        luxCap: 0,
+      },
+      allocated: {
+        con: 0,
+        dur: 0,
+        lux: 0,
+        conCap: 0,
+        durCap: 0,
+        luxCap: 0,
+      },
+      escrow: {
+        con: 0,
+        dur: 0,
+        lux: 0,
+        conCap: 0,
+        durCap: 0,
+        luxCap: 0,
+      },
+    };
+  }
 }
